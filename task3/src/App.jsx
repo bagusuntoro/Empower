f331dcfc987620a34de35de576f6385d3e437052
bagusuntoro <untorobagus65@gmail.com>
2023-02-24 14:18:49 +0700

    fixed design

--- task3/src/App.jsx
+++ task3/src/App.jsx
@@ -1,14 +1,13 @@
 import { useEffect, useState, useRef, useContext } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import {Layout} from './components/Layout'
+import { Layout } from './components/Layout'
 import { AuthContext } from './context/AuthContext'
 import { Login } from './components/Login'
 
-const Pricing = () =>{
+const Pricing = () => {
   const auth = useContext(AuthContext)
   console.log(auth)
-  return(
+  return (
     <h2>{auth.email ? 'Rp. 5000' : '-'}</h2>
   )
 }
@@ -19,32 +18,35 @@ function App() {
   const inputRef = useRef('')
 
   console.log('rendered ', input)
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault()
     console.log('Send :', inputRef.current.value)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     inputRef.current.focus()
-  },[])
+  }, [])
 
   return (
     <Layout>
       <div className="App">
-      <h1>Vite + React</h1>
-      <Login/>
-      
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input type="text" ref={inputRef} />
-        </div>
-        <br />
-        <button type='submit'>Send</button>
+        <h1>Login</h1>
 
+        <Login />
+
+
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input type="text" ref={inputRef} />
+          </div>
+          <br />
+          <button type='submit'>Send</button>
+        </form>
+        
         {/* <button onClick={()=> inputRef.current.focus()}>Focus</button> */}
-      </form>
-      <Pricing/>
-    </div>
+        <Pricing />
+      </div>
     </Layout>
   )
 }
--- task3/src/components/Login.jsx
+++ task3/src/components/Login.jsx
@@ -1,34 +1,33 @@
-import { useState } from "react";
+// import { useState, useEffect, useRef } from "react";
 import { Button, Checkbox, Form, Input } from 'antd';
 
-export const Login = () =>{
-
-    return(
+export const Login = () => {
+    return (
         <>
             <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
             >
-            <Input />
+                <Input />
             </Form.Item>
 
             <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
             >
-            <Input.Password />
+                <Input.Password />
             </Form.Item>
 
             <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-            <Checkbox>Remember me</Checkbox>
+                <Checkbox>Remember me</Checkbox>
             </Form.Item>
 
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-                Submit
-            </Button>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
             </Form.Item>
         </>
     )
