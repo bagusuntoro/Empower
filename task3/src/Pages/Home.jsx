2242d93b9f318578fac134f67b37e0cdcf8fb510
bagusuntoro <untorobagus65@gmail.com>
2023-03-12 20:42:55 +0700

    fixed axios

--- task3/src/Pages/Home.jsx
+++ task3/src/Pages/Home.jsx
@@ -1,19 +1,44 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import axios from 'axios'
+import { Table } from 'antd'
+
 // components
 import { TopNavigation } from '../components/header/TopNavigation'
 
-import { useNavigate } from 'react-router'
 
 export const Home = () => {
   const navigate = useNavigate()
+  const [kuis, setKuis] = useState([])
+
+  const getKuis = async () => {
+    try {
+      let response = await axios.get('https://opentdb.com/api.php?amount=7')
+      setKuis(response.data.results)
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
+  useEffect(() => {
+    getKuis();
+  }, [])
+
+  const columns = [
+    {
+      title: 'Quiz Question',
+      dataIndex: 'question',
+      key: 'question',
+    },
+  ];
+
   return (
     <>
       <TopNavigation />
       <div className="App">
-        {/* <Head /> */}
         <div className="template">
-          <div className="form">
-            <h2>HOMEPAGE</h2>
-          </div>
+          <h2>HOMEPAGE</h2>
+          <Table dataSource={kuis} columns={columns} />
         </div>
       </div>
     </>
